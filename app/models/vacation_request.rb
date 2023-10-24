037f56dad086911012a89246aa287d04c8bc22de
Rinat <rinenik3105@gmail.com>
2023-10-24 22:01:28 +0300

    Add new validation to dates in vac requests

--- app/models/vacation_request.rb
+++ app/models/vacation_request.rb
@@ -5,4 +5,6 @@ class VacationRequest < ApplicationRecord
 
   enum status: { pending: 0, approved: 1, rejected: 2 }
   validates :start_date, :end_date, presence: true
+  validates :end_date, comparison: { greater_than: Date.today }
+  validates :start_date, comparison: { less_than: :end_date }
 end
